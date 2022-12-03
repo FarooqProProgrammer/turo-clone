7a0559a4e61db9e28280861d245a1b5d4b00d515
FarooqProProgrammer <farooqayub646@gmail.com>
2022-12-03 22:40:31 +0500

    first

---
+++ src/components/Hero.jsx
@@ -0,0 +1,229 @@
+import React from 'react'
+import './styles/hero.css'
+function Hero() {
+  return (
+    <div>
+      <div class="seo-pages-bmnta1-Container-HeroContainer ea5l7to3 amino-inspecting">
+    <div class="seo-pages-1gdb0gc-DotScrim ea5l7to2"></div>
+    <div class="seo-pages-7qcdf9-StyledRow e1yh22tr0" data-testid="row">
+        <div class="seo-pages-13wc6fm-Container-StyledSearchContainer ea5l7to1">
+            <form autocomplete="off" data-testid="searchForm" class="seo-pages-1ed6tml-SearchFormContainer e1i6esey0">
+                <input type="hidden" value="disableAutocomplete" />
+                <div class="seo-pages-1vzvi65-FieldContainer e1nfc1bq1">
+                    <div class="seo-pages-1q0kmbx-StyledText">Where</div>
+                    <div class="seo-pages-1gt1ac9-InputContainer e1nfc1bq0">
+                        <div role="combobox" aria-expanded="false" aria-haspopup="listbox" aria-labelledby="searchFormLocationTypeahead-label" class="seo-pages-psa4jt-LocationTypeaheadContainer exl3yb91">
+                            <label for="searchFormLocationTypeahead-input" id="searchFormLocationTypeahead-label" class="seo-pages-1l6xaef-StyledLabel e3weq3j0">City, airport, address or hotel</label>
+                            <div class="seo-pages-bbvwm4-LocationInputContainer ea7zqv70">
+                                <input
+                                    class="seo-pages-om0pa0-StyledText"
+                                    aria-autocomplete="list"
+                                    aria-labelledby="searchFormLocationTypeahead-label"
+                                    autocomplete="off"
+                                    value=""
+                                    id="searchFormLocationTypeahead-input"
+                                    placeholder="City, airport, address or hotel"
+                                    data-testid="searchInput"
+                                />
+                            </div>
+                            <ul data-testid="suggestionList" role="listbox" aria-labelledby="searchFormLocationTypeahead-label" id="searchFormLocationTypeahead-menu" class="seo-pages-drosy0-SuggestionList exl3yb90"></ul>
+                        </div>
+                    </div>
+                </div>
+                <div class="seo-pages-1cyn4px-FieldContainer e1nfc1bq1">
+                    <div class="rangeDateTimePickerContainer seo-pages-yg651w-RangeDateTimePickerContainer e1ii6bye6">
+                        <div>
+                            <div class="seo-pages-z5t3ay-RangeDateTimePickerFields e1ii6bye5">
+                                <div class="seo-pages-1bq0tm0-DateTimeFieldContainer e1ii6bye4">
+                                    <div class="seo-pages-1q0kmbx-StyledText">From</div>
+                                    <div class="seo-pages-dpsq7c-DateTimeField e1ii6bye3">
+                                        <div class="DateInput searchFormRangeDateTimePicker-dateInput">
+                                            <input
+                                                class="seo-pages-jjrmab-StyledText DateInput_input needsclick"
+                                                aria-label="Start date"
+                                                autocomplete="off"
+                                                id="searchFormRangeDateTimePicker-dateRangePicker"
+                                                name="searchFormRangeDateTimePicker-dateRangePicker"
+                                                placeholder="Start date"
+                                                required=""
+                                                type="text"
+                                            />
+                                            <div class="DateInput_displayText DateInput_displayText__has-input">12/05/2022</div>
+                                            <svg width="16px" height="16px" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg" class="DateInput_icon seo-pages-1o6k71p" role="img" version="1.1">
+                                                <path
+                                                    fill-rule="evenodd"
+                                                    clip-rule="evenodd"
+                                                    d="M14 5.614a.75.75 0 0 1-.245.555l-5.25 4.773a.754.754 0 0 1-1.01 0l-5.25-4.773a.75.75 0 0 1 1.01-1.11L8 9.373l4.745-4.314A.75.75 0 0 1 14 5.614Z"
+                                                    fill="#121214"
+                                                ></path>
+                                            </svg>
+                                        </div>
+                                        <select aria-label="Start time" id="searchFormRangeDateTimePicker-startTime" class="seo-pages-1n0d4na-TimeSelect e1ii6bye2">
+                                            <option value="0">Midnight</option>
+                                            <option value="30">12:30 AM</option>
+                                            <option value="60">1:00 AM</option>
+                                            <option value="90">1:30 AM</option>
+                                            <option value="120">2:00 AM</option>
+                                            <option value="150">2:30 AM</option>
+                                            <option value="180">3:00 AM</option>
+                                            <option value="210">3:30 AM</option>
+                                            <option value="240">4:00 AM</option>
+                                            <option value="270">4:30 AM</option>
+                                            <option value="300">5:00 AM</option>
+                                            <option value="330">5:30 AM</option>
+                                            <option value="360">6:00 AM</option>
+                                            <option value="390">6:30 AM</option>
+                                            <option value="420">7:00 AM</option>
+                                            <option value="450">7:30 AM</option>
+                                            <option value="480">8:00 AM</option>
+                                            <option value="510">8:30 AM</option>
+                                            <option value="540">9:00 AM</option>
+                                            <option value="570">9:30 AM</option>
+                                            <option value="600">10:00 AM</option>
+                                            <option value="630">10:30 AM</option>
+                                            <option value="660">11:00 AM</option>
+                                            <option value="690">11:30 AM</option>
+                                            <option value="720">Noon</option>
+                                            <option value="750">12:30 PM</option>
+                                            <option value="780">1:00 PM</option>
+                                            <option value="810">1:30 PM</option>
+                                            <option value="840">2:00 PM</option>
+                                            <option value="870">2:30 PM</option>
+                                            <option value="900">3:00 PM</option>
+                                            <option value="930">3:30 PM</option>
+                                            <option value="960">4:00 PM</option>
+                                            <option value="990">4:30 PM</option>
+                                            <option value="1020">5:00 PM</option>
+                                            <option value="1050">5:30 PM</option>
+                                            <option value="1080">6:00 PM</option>
+                                            <option value="1110">6:30 PM</option>
+                                            <option value="1140">7:00 PM</option>
+                                            <option value="1170">7:30 PM</option>
+                                            <option value="1200">8:00 PM</option>
+                                            <option value="1230">8:30 PM</option>
+                                            <option value="1260">9:00 PM</option>
+                                            <option value="1290">9:30 PM</option>
+                                            <option value="1320">10:00 PM</option>
+                                            <option value="1350">10:30 PM</option>
+                                            <option value="1380">11:00 PM</option>
+                                            <option value="1410">11:30 PM</option>
+                                        </select>
+                                        <svg width="16px" height="16px" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg" class="DateInput_icon seo-pages-1o6k71p" role="img" version="1.1">
+                                            <path
+                                                fill-rule="evenodd"
+                                                clip-rule="evenodd"
+                                                d="M14 5.614a.75.75 0 0 1-.245.555l-5.25 4.773a.754.754 0 0 1-1.01 0l-5.25-4.773a.75.75 0 0 1 1.01-1.11L8 9.373l4.745-4.314A.75.75 0 0 1 14 5.614Z"
+                                                fill="#121214"
+                                            ></path>
+                                        </svg>
+                                    </div>
+                                </div>
+                                <div class="seo-pages-1bq0tm0-DateTimeFieldContainer e1ii6bye4">
+                                    <div class="seo-pages-1q0kmbx-StyledText">Until</div>
+                                    <div class="seo-pages-dpsq7c-DateTimeField e1ii6bye3">
+                                        <div class="DateInput searchFormRangeDateTimePicker-dateInput">
+                                            <input
+                                                class="seo-pages-jjrmab-StyledText DateInput_input needsclick"
+                                                aria-label="End date"
+                                                autocomplete="off"
+                                                id="searchFormRangeDateTimePicker-dateRangePicker-endDate"
+                                                name="searchFormRangeDateTimePicker-dateRangePicker-endDate"
+                                                placeholder="End date"
+                                                required=""
+                                                type="text"
+                                            />
+                                            <div class="DateInput_displayText DateInput_displayText__has-input">12/08/2022</div>
+                                            <svg width="16px" height="16px" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg" class="DateInput_icon seo-pages-1o6k71p" role="img" version="1.1">
+                                                <path
+                                                    fill-rule="evenodd"
+                                                    clip-rule="evenodd"
+                                                    d="M14 5.614a.75.75 0 0 1-.245.555l-5.25 4.773a.754.754 0 0 1-1.01 0l-5.25-4.773a.75.75 0 0 1 1.01-1.11L8 9.373l4.745-4.314A.75.75 0 0 1 14 5.614Z"
+                                                    fill="#121214"
+                                                ></path>
+                                            </svg>
+                                        </div>
+                                        <select aria-label="End time" id="searchFormRangeDateTimePicker-endTime" class="seo-pages-1n0d4na-TimeSelect e1ii6bye2">
+                                            <option value="0">Midnight</option>
+                                            <option value="30">12:30 AM</option>
+                                            <option value="60">1:00 AM</option>
+                                            <option value="90">1:30 AM</option>
+                                            <option value="120">2:00 AM</option>
+                                            <option value="150">2:30 AM</option>
+                                            <option value="180">3:00 AM</option>
+                                            <option value="210">3:30 AM</option>
+                                            <option value="240">4:00 AM</option>
+                                            <option value="270">4:30 AM</option>
+                                            <option value="300">5:00 AM</option>
+                                            <option value="330">5:30 AM</option>
+                                            <option value="360">6:00 AM</option>
+                                            <option value="390">6:30 AM</option>
+                                            <option value="420">7:00 AM</option>
+                                            <option value="450">7:30 AM</option>
+                                            <option value="480">8:00 AM</option>
+                                            <option value="510">8:30 AM</option>
+                                            <option value="540">9:00 AM</option>
+                                            <option value="570">9:30 AM</option>
+                                            <option value="600">10:00 AM</option>
+                                            <option value="630">10:30 AM</option>
+                                            <option value="660">11:00 AM</option>
+                                            <option value="690">11:30 AM</option>
+                                            <option value="720">Noon</option>
+                                            <option value="750">12:30 PM</option>
+                                            <option value="780">1:00 PM</option>
+                                            <option value="810">1:30 PM</option>
+                                            <option value="840">2:00 PM</option>
+                                            <option value="870">2:30 PM</option>
+                                            <option value="900">3:00 PM</option>
+                                            <option value="930">3:30 PM</option>
+                                            <option value="960">4:00 PM</option>
+                                            <option value="990">4:30 PM</option>
+                                            <option value="1020">5:00 PM</option>
+                                            <option value="1050">5:30 PM</option>
+                                            <option value="1080">6:00 PM</option>
+                                            <option value="1110">6:30 PM</option>
+                                            <option value="1140">7:00 PM</option>
+                                            <option value="1170">7:30 PM</option>
+                                            <option value="1200">8:00 PM</option>
+                                            <option value="1230">8:30 PM</option>
+                                            <option value="1260">9:00 PM</option>
+                                            <option value="1290">9:30 PM</option>
+                                            <option value="1320">10:00 PM</option>
+                                            <option value="1350">10:30 PM</option>
+                                            <option value="1380">11:00 PM</option>
+                                            <option value="1410">11:30 PM</option>
+                                        </select>
+                                        <svg width="16px" height="16px" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg" class="DateInput_icon seo-pages-1o6k71p" role="img" version="1.1">
+                                            <path
+                                                fill-rule="evenodd"
+                                                clip-rule="evenodd"
+                                                d="M14 5.614a.75.75 0 0 1-.245.555l-5.25 4.773a.754.754 0 0 1-1.01 0l-5.25-4.773a.75.75 0 0 1 1.01-1.11L8 9.373l4.745-4.314A.75.75 0 0 1 14 5.614Z"
+                                                fill="#121214"
+                                            ></path>
+                                        </svg>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <button type="submit" class="seo-pages-ie63rs-SearchButton eixhdi72">
+                    <span id="searchIconLabel" class="seo-pages-1qi7ygy-SearchButtonText eixhdi71">Search for cars</span>
+                    <span class="seo-pages-wyg57s-TabletAndGreaterIcon eixhdi70">
+                        <svg width="24px" height="24px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" aria-labelledby="searchIconLabel" class="seo-pages-1iuz9hb" role="img" version="1.1">
+                            <path
+                                d="M20.75 21.52a.62.62 0 0 1-.44-.18l-6.11-6.11a.627.627 0 0 1-.18-.45c0-.17.07-.33.19-.45l.08-.07c2.51-2.51 2.51-6.61-.01-9.12-2.52-2.52-6.62-2.52-9.13 0-2.52 2.52-2.52 6.62 0 9.13a6.471 6.471 0 0 0 6.29 1.66c.34-.09.68.1.77.44.09.33-.1.68-.44.77-2.67.74-5.55-.02-7.51-1.98-3-3.01-3-7.9 0-10.9s7.9-3.01 10.9 0c2.88 2.88 3 7.5.35 10.52l5.68 5.68c.24.24.24.64 0 .88a.62.62 0 0 1-.44.18Z"
+                                fill="#121214"
+                            ></path>
+                        </svg>
+                    </span>
+                </button>
+            </form>
+        </div>
+    </div>
+</div>
+
+    </div>
+  )
+}
+
+export default Hero
